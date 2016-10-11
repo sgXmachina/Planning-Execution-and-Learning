7d64509ac3b2207ed73688546bf8079ca5d9cf43
sgautam <shivamgautam@gmail.com>
2016-10-11 17:11:31 -0400

    final submission

--- lunar_lockout/planner.cpp
+++ lunar_lockout/planner.cpp
@@ -6,6 +6,7 @@
 #include <cstring>
 
 #include "planners.h"
+#include <time.h>
 
 
 
@@ -21,12 +22,12 @@ int main(int argc,char* argv[0])
 		std::cout<<"Invalid Number of arguments.\n";
 		std::cout<<"Usage is ./a.out <domain_name> <puzzle_number>";
 		std::cout<<"Valid Arguements are domain1 (for spaceship representation) and domain2 (for grid representation)\n";
-		std::cout<<"Valid puzzle numbers are: 9 ,18, 27, 36\n";
+		std::cout<<"Valid puzzle numbers are: 1 ,2, 3, 4\n";
 	}
 
 	else
 	{	
-		if (strlen(argv[2])>2)	
+		if (strlen(argv[2])>1)	
 		{
 			std::cout<<"Invalid Puzzle Number\n";
 		}
@@ -34,11 +35,15 @@ int main(int argc,char* argv[0])
 		else
 		{
 		int puzzle_number = argv[2][0]-'0';
-		std::cout<<puzzle_number;
+		clock_t t1,t2;
+		t1=clock();
 		if (!strcmp(argv[1],"domain1"))  
 				init_planning_spaceship_statespace(puzzle_number);
 		if (!strcmp(argv[1],"domain2"))  
 				init_planning_cell_statespace(puzzle_number);
+		t2=clock();
+		std::cout<<"\nTime taken: "<<((float)t2 - (float)t1)/CLOCKS_PER_SEC<<std::endl;
 		}
+
 	}
 }
